bbd1f6f048eb3e1aaa64011a9ec194bf03561ec6
Christian Wallervand <cwallervand@gmail.com>
2016-08-17 15:34:23 +0200

    Made code splitting work

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -22,7 +22,15 @@ var config = {
         exclude: /nodes_modules/ //A RegEx to tell the loader which files to exclude
       }
     ]
-  }
+  },
+  //Optimize vendor chunk. This removes alle the modules
+  //This will remove all modules in the vendor chunk from the app chunk. The bundle.js will now contain just your app code, without any of its dependencies. These are in vendor.bundle.js.
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      fileName: 'vendor.bundle.js',
+    })
+  ]
 };
 
 module.exports = config;
